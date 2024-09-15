74ce8542ee1cee3ab112ffae9a1e2d9872ba46a6
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2024-09-15 17:11:08 +0900

    [add] Add files

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI(
+    title="Translator",
+    version="1.0.0",
+    redoc_url=None,
+    root_path="/translator")
+
+@app.get("/")
+async def index(request: Request):
+    client_host = request.client.host
+    return JSONResponse({"client_host": client_host})
+
+if __name__ == '__main__':
+    uvicorn.run(app=app)
