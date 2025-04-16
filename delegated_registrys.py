97eed51bc622bbca2a14a113c50e30e1b4954a30
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-04-17 08:56:38 +0900

    [modify] Update delegated_registrys.py

--- delegated_registrys.py
+++ delegated_registrys.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.12
 
 import ipaddress
-import math
+import os
 import requests
 import polars as pl
 
@@ -14,26 +14,52 @@ REGISTRYS: dict[str, str] = {
 }
 COLUMN_NAMES: list[str] = ["network_address", "registry", "country_code", "type", "start", "value", "date", "status"]
 
-def calculate_ipv4_cidr_prefix(ip_count: int) -> int:
-    """IPv4アドレスの数からCIDRプレフィックスを計算する"""
-    if ip_count <= 0 or (ip_count & (ip_count - 1)) != 0:
-        raise ValueError("IPv4アドレスの数は2のべき乗でなければなりません。")
-    return 32 - int(math.log2(ip_count))
+def calculate_ipv4_networks(start_ip, ip_count: int) -> list:
+    networks = []
+    remaining_ips = ip_count
+    current_ip = start_ip
+
+    while remaining_ips > 0:
+        # 残りのIP数に収まる最大の2のべき乗を計算
+        max_prefix = 32 - (remaining_ips - 1).bit_length()
+        try:
+            network = ipaddress.ip_network(f"{current_ip}/{max_prefix}", strict=True)
+        except ValueError:
+            pass
+            # IPアドレスが無効な場合、サイズを小さくして再計算
+            for max_prefix in range(max_prefix+1, 32, 1):
+                try:
+                    network = ipaddress.ip_network(f"{current_ip}/{max_prefix}", strict=True)
+                    break
+                except ValueError:
+                    continue
+
+        networks.append(network)
+
+        # 次のネットワークの開始アドレスを計算
+        remaining_ips -= network.num_addresses
+        current_ip = network[-1] + 1
+
+    return networks
 
 def main():
     for nic_name, url in REGISTRYS.items():
         #print(nic_name, url)
 
         data = None
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.text
-        except:
-            continue
+        if os.path.isfile(f"./nic/{nic_name}"):
+            with open(f"./nic/{nic_name}", "r") as fp:
+                data = fp.read()
+        else:
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.text
+            except:
+                continue
 
         lines = data.strip().split("\n")
-        addresses = []
+        records = []
         for line in lines:
             if line.startswith("#"):
                 continue
@@ -53,24 +79,26 @@ def main():
             if row[2] == "asn":
                 continue
 
-            network = None
+            networks = []
             if row[2] == "ipv4":
-                try:
-                    prefix = calculate_ipv4_cidr_prefix(int(row[4]))
-                    network = ipaddress.ip_network(f"{row[3]}/{prefix}")
-                except ValueError:
-                    network = None
+                networks = calculate_ipv4_networks(ipaddress.IPv4Address(row[3]),int(row[4]))
             elif row[2] == "ipv6":
-                try:
-                    network = ipaddress.ip_network(f"{row[3]}/{row[4]}")
-                except ValueError:
-                    network = None
+                networks = [ipaddress.ip_network(f"{row[3]}/{row[4]}", strict=False)]
 
-            row.insert(0, str(network))
+            for network in networks:
+                data = [
+                    f"{str(network.network_address)}/{str(network.prefixlen)}",
+                    row[0],  # registry
+                    row[1],  # country_code
+                    row[2],  # type
+                    str(network.network_address),  # start
+                    network.num_addresses if row[2] == "ipv4" else None,  # value
+                    row[5],  # date
+                    row[6],  # status
+                ]
+                records.append(data)
 
-            addresses.append(row[0:8])
-
-        df = pl.DataFrame(addresses, schema=COLUMN_NAMES, orient="row")
+        df = pl.DataFrame(records, schema=COLUMN_NAMES, orient="row")
         df.write_csv(f"registry_{nic_name}.csv")
 
 if __name__ == "__main__":
